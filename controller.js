2a76896dbe223aadcd482378e504bb9246ca122e
izaaklb <izaakbrindle@gmail>
2023-01-20 13:51:55 +0000

    Revert "task 8"
    This reverts commit e865d38356057c97195f66cf9bdde408a7b041da.

--- controller.js
+++ controller.js
@@ -4,7 +4,6 @@ const {
   fetchReviewById,
   fetchCommentsByReviewId,
   insertComment,
-  incrementVotes,
 } = require("./model");
 
 getCategories = (request, response, next) => {
@@ -51,21 +50,10 @@ postComment = (request, response, next) => {
     .catch(next);
 };
 
-patchVotes = (request, response, next) => {
-  const reviewId = request.params;
-  const voteIncrement = request.body;
-  incrementVotes(reviewId, voteIncrement)
-    .then((updatedReview) => {
-      response.status(202).send(updatedReview);
-    })
-    .catch(next);
-};
-
 module.exports = {
   getCategories,
   getReviews,
   getReviewById,
   getCommentsByReviewId,
   postComment,
-  patchVotes,
 };
